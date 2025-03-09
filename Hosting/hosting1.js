274f1fc7a7797dfe9a824adb7d9472f5a7017cac
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2025-03-09 15:39:07 +0530

    Hoisting

--- Hosting/hosting1.js
+++ Hosting/hosting1.js
@@ -9,13 +9,42 @@ console.log("value of age", age); //here we initlized with 30 so that it print 3
 
 
 // Question 2.    hosting with let and const
-
+/*
 age1 = 100;
 console.log("value of age is", age1) // we got a refrenece Error due to Temporal Dead Zone
 let age1 = 30;
 console.log("value of age is", age1);
 // Reason
-/*
+
 When we declare the variable with let and const they are not initialized immediately.
 The period from start of the scope untill the variable is declared is the Temperol dead zone,if try to acess they give you Reffrence Error
 */
+
+// Question 3. hosting with functiin declration
+
+var x = 1;
+
+function foo() {
+
+    x = 10;
+
+    return;
+
+    function x() {
+
+    }
+
+}
+
+foo();
+
+console.log(x)  // output:1
+
+/* Explaination 
+When the javascript engine first time parse the script var x is initlize with unfined and then function is Foo is hoisted.
+Now Inside  the foo function, function x is also hosited at top of their local scope inside foo function when the execution of foo function is start then,
+  
+     he assignment x = 10; modifies the local x, which is actually a function, not the global x.
+     then return  from the function without altring the global variable x.
+     That's why the output is 1 
+     */
