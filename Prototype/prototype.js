fe0a41867edf2a2f5161d0ff80cf141d1f0c4ba9
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2025-03-11 22:46:18 +0530

    pratice3

--- Prototype/prototype.js
+++ Prototype/prototype.js
@@ -34,3 +34,52 @@ Even though getIntro is not defined in obj2, it can still access it from obj via
 This concept of inheriting properties and methods from another object is called Prototypical Inheritance.
 
 */
+
+
+// Question 1
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.greet = function () {
+    return "Hello, " + this.name;
+};
+
+const user = new Person("Mohit");
+console.log(user.greet());  // Output "Hello Mohit"
+console.log(user.__proto__ === Person.prototype); // true
+console.log(Person.prototype.__proto__ === Object.prototype); // true
+console.log(Object.prototype.__proto__); // null
+
+
+// Question2  // instance level prototyoical overriding
+
+function Animal() { }
+
+Animal.prototype.makeSound = function () {
+    return "Generic sound";
+};
+
+let cat = new Animal();
+console.log(cat.__proto__);
+cat.makeSound = function () {
+    return "Meow";
+};
+
+console.log(cat.makeSound()); // "Meow"
+delete cat.makeSound;
+console.log(cat.makeSound()); // "Generic sound"
+
+// Question 3  Prototypical inheritance
+
+let obj3 = {
+    a: 10,
+    b: 20
+};
+
+let obj4 = Object.create(obj3);
+obj4.c = 30;
+
+console.log(obj4.a, obj4.b, obj4.c); // 10,20,30
+console.log(obj4.hasOwnProperty("a"))// false
+console.log(obj4.hasOwnProperty("c")); // true
